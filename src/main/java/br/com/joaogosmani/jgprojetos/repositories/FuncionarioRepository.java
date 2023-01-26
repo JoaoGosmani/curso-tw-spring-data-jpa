4c5db7ab386ce731c0e1c0333fb500e133b04de8
Joao Gosmani <joaogosmani@gmail.com>
2023-01-25 21:14:54 -0300

    Usando a annotation EntityGraph para resolver o problema de N+1

--- src/main/java/br/com/joaogosmani/jgprojetos/repositories/FuncionarioRepository.java
+++ src/main/java/br/com/joaogosmani/jgprojetos/repositories/FuncionarioRepository.java
@@ -1,9 +1,15 @@
 package br.com.joaogosmani.jgprojetos.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.joaogosmani.jgprojetos.models.Funcionario;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+    @EntityGraph(attributePaths = {"endereco", "cargo"})
+    List<Funcionario> findAll();
 
 }
